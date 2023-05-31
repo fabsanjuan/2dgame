c0312fb8c7d7a17f3fb91b7b86dd323028728b27
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2023-05-31 18:34:01 +0000

    Add obstacle and player basic collision detection/physics

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ window.addEventListener('load', function(){
     ctx.strokeStyle = 'white';
 
     //object oriented to make game modular. Player and game class to manage game logic.
-
-    
     class Player {
         constructor(game){
             this.game = game; //This gives the player access to the entire game class (prooperties and methods).
@@ -21,13 +19,22 @@ window.addEventListener('load', function(){
             this.speedY = 0;
             this.dx = 0;
             this.dy = 0;
-            this.speedModifier = 15;
+            this.speedModifier = 4;
+            this.spriteWidth = 255;
+            this.spriteHeight = 255;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX;
+            this.spriteY;
+            this.frameX = 0;
+            this.frameY = 5;
+            this.image = document.getElementById('bull');
 
         };
-
         draw(context){  //draws and animates the player. Expects arg to specify which canvas to draw on.
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
             context.beginPath();
-            context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
+            context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
             context.save(); //save and restore methods allow us to apply specific drawing settings to selected shapes without affecting others (wrap).
             context.globalAlpha = 0.5;
             context.fill();
@@ -40,8 +47,19 @@ window.addEventListener('load', function(){
 
         };
         update(){
-            this.dx = this.game.mouse.x - this.collisionX;
-            this.dy = this.game.mouse.y - this.collisionY;
+            this.dx = this.game.mouse.x - this.collisionX; //Horizontal distance between mouse and sprite.
+            this.dy = this.game.mouse.y - this.collisionY; //Veritcal distance between mouse and sprite.
+            //sprite animation depending on angle between sprite and mouse.
+            const angle = Math.atan2(this.dy, this.dx);
+            if (angle < -2.74 || angle > 2.74){this.frameY = 6}
+            else if (angle < -1.96){this.frameY = 7}
+            else if (angle < -1.17){this.frameY = 0}
+            else if (angle < -0.39){this.frameY = 1}
+            else if (angle < 0.39){this.frameY = 2}
+            else if (angle < 1.17){this.frameY = 3}
+            else if (angle < 1.96){this.frameY = 4}
+            else if (angle < 2.74){this.frameY = 5};
+
             const distance = Math.hypot(this.dy, this.dx);
             if (distance > this.speedModifier){ //only move the player if the distance is greater than speed mod.
                 this.speedX = this.dx/distance || 0;
@@ -52,6 +70,18 @@ window.addEventListener('load', function(){
             }
             this.collisionX += this.speedX * this.speedModifier;
             this.collisionY += this.speedY * this.speedModifier;
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 100;
+            //collisions with obstacles
+            this.game.obstacles.forEach(obstacle => {
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, obstacle); //JavaScript destructuring.
+                if (collision){ //Technique to push player circle away from obstacle circle, simple physics sim.
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
+                }
+            });
         };
 
     };
@@ -69,9 +99,11 @@ window.addEventListener('load', function(){
             this.height = this.spriteHeight;
             this.spriteX = this.collisionX - this.width * 0.5;
             this.spriteY = this.collisionY - this.height * 0.5 - 70;
+            this.frameX = Math.floor(Math.random() * 4);
+            this.frameY = Math.floor(Math.random() * 3);
         };
         draw(context){
-            context.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
             context.beginPath();
             context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
             context.save(); //save and restore methods allow us to apply specific drawing settings to selected shapes without affecting others (wrap).
@@ -86,7 +118,8 @@ window.addEventListener('load', function(){
         constructor(canvas){
             this.canvas = canvas;
             this.width = this.canvas.width; //takes reference to canvas element and sets height and width of the game to match canvas element.
-            this.height = this.canvas.height; //
+            this.height = this.canvas.height;
+            this.topMargin = 260;
             this.player = new Player(this); //this auto creates player when creating an instance of game.
             this.obstacles = [];
             this.numberOfObstacles = 5;
@@ -115,9 +148,16 @@ window.addEventListener('load', function(){
             });
         };
         render(context){  //draws and update all objects in the game.
+            this.obstacles.forEach(obstacle => obstacle.draw(context));
             this.player.draw(context);
             this.player.update();
-            this.obstacles.forEach(obstacle => obstacle.draw(context));
+        };
+        checkCollision(a, b){ //Check the distance between 2 spheres and see if radii overlap.
+            const dx = a.collisionX - b.collisionX;
+            const dy = a.collisionY - b.collisionY;
+            const distance = Math.hypot(dy, dx);
+            const sumOfRadii = a.collisionRadius + b.collisionRadius;
+            return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]; //returns useful information about collision physics between objects.    
         };
         init(){
             let attempts = 0;
@@ -128,12 +168,14 @@ window.addEventListener('load', function(){
                     const dx = testObstacle.collisionX - obstacle.collisionX;
                     const dy = testObstacle.collisionY - obstacle.collisionY;
                     const distance = Math.hypot(dy, dx);
-                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius;
+                    const distanceBuffer = 100; //Adds space between the obstacles for player navigation.
+                    const sumOfRadii = testObstacle.collisionRadius + obstacle.collisionRadius + distanceBuffer;
                     if (distance < sumOfRadii){
                         overlap = true;
                     };
                 });
-                if (!overlap){
+                const margin =  testObstacle.collisionRadius * 2; //Adds margins to the edges of the game field for player navigation
+                if (!overlap && testObstacle.spriteX > 0 && testObstacle.spriteX < this.width - testObstacle.width && testObstacle.collisionY > this.topMargin + margin && testObstacle.collisionY < this.height - margin){
                     this.obstacles.push(testObstacle);
                 };
                 attempts++;
