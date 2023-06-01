c05b3ad141d1f41815b6cc799d90d5512ac9e585
Fabio Sanjuan <fabio.sanjuangomez@gmail.com>
2023-06-01 19:09:59 +0000

    Add egg class and drawing/rendering order of game objects

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ window.addEventListener('load', function(){
                     const unit_y = dy / distance;
                     this.collisionX = obstacle.collisionX + (sumOfRadii + 1) * unit_x;
                     this.collisionY = obstacle.collisionY + (sumOfRadii + 1) * unit_y;
-                }
+                };
             });
         };
 
@@ -123,6 +123,52 @@ window.addEventListener('load', function(){
             context.stroke();
             };            
         };
+        update(){
+
+        };
+    };
+
+    class Egg {
+        constructor(game){ //Object for eggs spawning at random locations within game boundaries.
+            this.game = game;
+            this.collisionRadius = 40;
+            this.margin = this.collisionRadius * 2;
+            this.collisionX = this.margin + (Math.random() * (this.game.width - this.margin * 2));
+            this.collisionY = this.game.topMargin + (Math.random() * (this.game.height - this.game.topMargin - this.margin));
+            this.image = document.getElementById('egg');
+            this.spriteWidth = 110;
+            this.spriteHeight = 135;
+            this.width = this.spriteWidth;
+            this.height = this.spriteHeight;
+            this.spriteX;
+            this.spriteY;
+        };
+        draw(context){
+            context.drawImage(this.image, this.spriteX, this.spriteY);
+            if (this.game.debug){
+                context.beginPath();
+                context.arc(this.collisionX,this.collisionY,this.collisionRadius,0,Math.PI * 2);
+                context.save(); //save and restore methods allow us to apply specific drawing settings to selected shapes without affecting others (wrap).
+                context.globalAlpha = 0.5;
+                context.fill();
+                context.restore();
+                context.stroke();
+                };
+        };
+        update(){
+            this.spriteX = this.collisionX - this.width * 0.5;
+            this.spriteY = this.collisionY - this.height * 0.5 - 30;
+            let collisionObjects = [this.game.player, ...this.game.obstacles]; //spread operator ... 
+            collisionObjects.forEach( object => {
+                let [collision, distance, sumOfRadii, dx, dy] = this.game.checkCollision(this, object);
+                if (collision){
+                    const unit_x = dx / distance;
+                    const unit_y = dy / distance;
+                    this.collisionX = object.collisionX + (sumOfRadii + 1) * unit_x;
+                    this.collisionY = object.collisionY + (sumOfRadii + 1) * unit_y;
+                }
+            });
+        };
     };
     
     class Game {
@@ -136,8 +182,13 @@ window.addEventListener('load', function(){
             this.fps = 70;
             this.timer = 0;
             this.interval = 1000/this.fps;
+            this.eggTimer = 0;
+            this.eggInterval = 1000;
+            this.eggs = [];
+            this.maxEggs = 10;
             this.obstacles = [];
             this.numberOfObstacles = 5;
+            this.gameObjects = [];
             this.mouse = {
                 x: this.width * 0.5,
                 y: this.height * 0.5,
@@ -168,12 +219,25 @@ window.addEventListener('load', function(){
         render(context, deltaTime){  //draws and update all objects in the game.
             if (this.timer > this.interval){
                 ctx.clearRect(0, 0, this.width, this.height);
-                this.obstacles.forEach(obstacle => obstacle.draw(context));
-                this.player.draw(context);
-                this.player.update();
+                this.gameObjects = [...this.eggs, ...this.obstacles, this.player];
+                this.gameObjects.sort((a, b) => { //sorts drawn objects by vertial position.
+                    return a.collisionY - b.collisionY;
+                });
+                this.gameObjects.forEach(object => {
+                    object.draw(context);
+                    object.update();
+                });
                 this.timer = 0;
             };
             this.timer += deltaTime;
+
+            //periodically add eggs.
+            if (this.eggTimer > this.eggInterval && this.eggs.length < this.maxEggs){
+                this.addEgg();
+                this.eggTimer = 0;
+            } else {
+                this.eggTimer += deltaTime;
+            }
         };
         checkCollision(a, b){ //Check the distance between 2 spheres and see if radii overlap.
             const dx = a.collisionX - b.collisionX;
@@ -182,6 +246,10 @@ window.addEventListener('load', function(){
             const sumOfRadii = a.collisionRadius + b.collisionRadius;
             return [(distance < sumOfRadii), distance, sumOfRadii, dx, dy]; //returns useful information about collision physics between objects.    
         };
+        addEgg(){
+            this.eggs.push(new Egg(this));
+        };
+
         init(){
             let attempts = 0;
             while (this.obstacles.length < this.numberOfObstacles && attempts < 500){
